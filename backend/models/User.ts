30d04e1db4c37aabceee76dd9735860650daa1f4
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-30 17:37:04 +0200

    Update User model to accept user roles and hash passwords upon user creation

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,15 +1,44 @@
+import bcrypt from "bcryptjs"
 import mongoose, { Document } from "mongoose"
 
 export interface IUser extends Document {
   name: string
   email: string
   password: string
+  role: string
 }
 
 const userSchema = new mongoose.Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["reader", "author", "publisher"],
+    default: "reader",
+  },
 })
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return await bcrypt.compare(candidatePassword, this.password)
+}
 
 export default mongoose.models.User || mongoose.model<IUser>("User", userSchema)
