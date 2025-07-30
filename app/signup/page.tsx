2ca3f74431d8071f87e23ea5e23d341c3eeb9e20
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-30 17:43:10 +0200

    Update signup page to include user role inputs, and send it to the backend

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -9,6 +9,7 @@ const SignUpPage = () => {
 
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
+  const [userRole, setUserRole] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState("")
@@ -18,12 +19,12 @@ const SignUpPage = () => {
     e.preventDefault()
     setError("")
 
-    if (!name || !email || !password) {
+    if (!name || !email || !password || !userRole) {
       setError("All fields are required!")
       return
     }
 
-    console.log({ name, email, password, confirmPassword })
+    console.log({ name, email, userRole, password, confirmPassword })
 
     if (password !== confirmPassword) {
       setError("Passwords don't match!")
@@ -31,7 +32,7 @@ const SignUpPage = () => {
     }
 
     try {
-      await signup(name, email, password)
+      await signup(name, email, userRole, password)
 
       router.push("/")
     } catch (err: any) {
@@ -44,7 +45,7 @@ const SignUpPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-8">
         <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">
-          Create Account
+          Create Your Account
         </h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
@@ -61,6 +62,32 @@ const SignUpPage = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-700"
           />
+
+          {/* User Type Radio Buttons */}
+          <div>
+            <h1 className="text-md font-semibold text-gray-900 mb-3">
+              Join SmartReads as a:
+            </h1>
+            <div className="flex justify-between gap-2">
+              {["reader", "author", "publisher"].map((type) => (
+                <label
+                  key={type}
+                  className="flex items-center gap-2 p-3 border rounded-lg cursor-pointer w-full text-sm font-medium text-gray-700 border-gray-300"
+                >
+                  <input
+                    type="radio"
+                    name="userType"
+                    value={type}
+                    checked={userRole === type}
+                    onChange={() => setUserRole(type)}
+                    className="accent-gray-700"
+                  />
+                  {type.charAt(0).toUpperCase() + type.slice(1)}
+                </label>
+              ))}
+            </div>
+          </div>
+
           <input
             type="password"
             placeholder="Password"
