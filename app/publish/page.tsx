52adf90de53cc4095280074689aacec944c85ad7
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-31 22:38:59 +0200

    Update publish page to load depending on user.role

--- app/publish/page.tsx
+++ app/publish/page.tsx
@@ -1,22 +1,30 @@
 "use client"
 
 import { useEffect } from "react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useAuth } from "../context/AuthContext"
 import PublishBookForm from "@/components/PublishBookForm"
 
 export default function PublishBookPage() {
-  const { user } = useAuth()
-  //   const router = useRouter()
-
-  user && console.log("userRole: ", user.role)
+  const { user, loading } = useAuth()
+  const router = useRouter()
 
   useEffect(() => {
-    if (!user || (user.role !== "author" && user.role !== "publisher")) {
-      //   router.replace("/")
-      user && console.log("userRole: ", user.role)
+    if (!loading) {
+      if (!user || (user.role !== "author" && user.role !== "publisher")) {
+        console.log("Redirecting because user role is:", user?.role)
+        router.replace("/")
+      }
     }
-  }, [user])
+  }, [user, loading])
+
+  const LoadingSpinner = () => (
+    <div className="flex justify-center items-center h-[70vh]">
+      <div className="w-16 h-16 border-4 border-[#c8553dff] border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  )
+
+  if (loading) return <LoadingSpinner />
 
   if (!user || (user.role !== "author" && user.role !== "publisher")) {
     return null
