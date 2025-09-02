a52a910dd65061e788735abfde25d4132ff0a6b0
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-02 11:39:30 +0200

    Include images domain in next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["assets.production.scribd.com", "images.pexels.com"],
+    domains: [
+      "assets.production.scribd.com",
+      "images.pexels.com",
+      "res.cloudinary.com",
+    ],
   },
 }
 
