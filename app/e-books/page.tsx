426a0ab9f0c84bd403ddcfebddb74000d3f8e3f7
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-02 11:31:18 +0200

    Update e-books layout and include a search form

--- app/e-books/page.tsx
+++ app/e-books/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import React, { useEffect, useState } from "react"
+import { bookCategories } from "@/components/PublishBookForm"
 
 type Book = {
   _id: string
@@ -16,8 +17,10 @@ type Book = {
 
 export default function AllBooksPage() {
   const [books, setBooks] = useState<Book[]>([])
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState("")
+  const [activeCategory, setActiveCategory] = useState("All Categories")
 
   const fetchAllBooks = async () => {
     try {
@@ -32,8 +35,10 @@ export default function AllBooksPage() {
       }
       const data = await response.json()
       setBooks(data.books)
+      setFilteredBooks(data.books)
     } catch (err: any) {
-      setError(err.message || "Something went wrong")
+      // setError(`Something went wrong: ${err.message}`)
+      setError("No Books Found")
     } finally {
       setLoading(false)
     }
@@ -43,19 +48,26 @@ export default function AllBooksPage() {
     fetchAllBooks()
   }, [])
 
-  console.log("Fetched books:", books)
+  useEffect(() => {
+    if (activeCategory === "All Categories" || activeCategory === "") {
+      setFilteredBooks(books)
+    } else {
+      const filtered = books.filter((book) => book.category === activeCategory)
+      setFilteredBooks(filtered)
+    }
+  }, [activeCategory, books])
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-xl">Loading books...</p>
+      <div className="flex justify-center items-center mt-20 h-screen">
+        <div className="w-16 h-16 border-4 border-[#c8553d] border-t-transparent rounded-full animate-spin"></div>
       </div>
     )
   }
 
   if (error) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center mt-20 h-60">
         <p className="text-red-600 text-xl">{error}</p>
       </div>
     )
@@ -63,12 +75,40 @@ export default function AllBooksPage() {
 
   return (
     <section className="max-w-6xl mx-auto px-4 py-12 bg-[#fdfdfd]">
-      <h1 className="text-4xl font-bold text-[#c8553d] mb-10 text-center">
-        E-Books Library
-      </h1>
+      <div className="flex flex-col items-center space-y-3.5">
+        <h1 className="text-3xl font-bold text-[#c8553d]">E-Books Library</h1>
+        <p className="text-lg font-medium">What interests you</p>
+      </div>
+      <div className="flex items-center justify-between mb-8 py-10 px-6">
+        <form action="">
+          <input
+            type="text"
+            placeholder="Search books, authors, genres..."
+            className="max-md:hidden md:w-xs lg:w-lg rounded-full border border-black px-5 py-2 text-gray-600"
+          />
+        </form>
+
+        <div>
+          <label htmlFor="category" className="text-xl mr-2">
+            Filter books:
+          </label>
+          <select
+            id="category"
+            value={activeCategory}
+            onChange={(e) => setActiveCategory(e.target.value)}
+            className="ml-4 p-2 border rounded-md"
+          >
+            {bookCategories.map((category) => (
+              <option key={category.value} value={category.value}>
+                {category.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <div
             key={book._id}
             className="bg-white rounded-xl border border-[#f3f3f3] shadow-sm hover:shadow-md transition duration-300 overflow-hidden"
