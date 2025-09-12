ad2d934b546abeb4694bc02cc7af2fc5f885e36e
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-12 08:32:08 +0200

    Create dropdown user menu

---
+++ components/uiComponent/UserDropdown.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+import ProfileAvatar from "./ProfileAvatar"
+import Link from "next/link"
+import LogoutButton from "@/components/uiComponent/LogoutButton"
+
+const UserDropdown = () => {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>
+          <ProfileAvatar />
+        </MenubarTrigger>
+        <MenubarSeparator />
+        <MenubarContent>
+          <MenubarItem>
+            <Link href="/profile" className="w-full">
+              Profile
+            </Link>
+          </MenubarItem>
+          <MenubarItem>
+            <LogoutButton />
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  )
+}
+
+export default UserDropdown
