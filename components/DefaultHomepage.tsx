64ce1b2819c78cb9c54b0b35855eea222257e61b
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-27 15:48:59 +0200

    Add skeleton in the Default homepage

--- components/DefaultHomepage.tsx
+++ components/DefaultHomepage.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image"
-import Link from "next/link"
 import React, { useEffect, useState } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
 import BooksComponent from "./BooksComponent"
 import RecommendedCategories from "./RecommendedCategories"
 import BookSlider from "./BooksSlider"
@@ -64,27 +63,134 @@ const ReaderHomepage = () => {
       <BookSlider />
 
       {/* Best Sellers */}
-      <section className="p-5 max-w-5xl mx-auto">
-        <BooksComponent
-          title="Bestselling Books & Audiobooks"
-          subTitle=""
-          books={books as Book[]}
-        />
-      </section>
+      {loading ? (
+        <div className="block w-fit mx-auto my-10">
+          <div className="flex flex-col gap-3 p-5">
+            <div className="mb-2 space-y-3">
+              <Skeleton className="h-6 w-[300px]" />
+              <Skeleton className="h-4 w-[250px]" />
+            </div>
+            <div className="flex items-center justify-center gap-3">
+              <div className="space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden md:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden sm:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden lg:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <section className="p-5 max-w-5xl mx-auto">
+          <BooksComponent
+            title="Bestselling Books & Audiobooks"
+            subTitle=""
+            books={books as Book[]}
+          />
+        </section>
+      )}
 
-      <section className="p-5 max-w-5xl mx-auto">
-        <BooksComponent
-          title="Current New York Times Bestsellers"
-          subTitle="
+      {loading ? (
+        <div className="block w-fit mx-auto my-10">
+          <div className="flex flex-col gap-3 p-5">
+            <div className="mb-2 space-y-3">
+              <Skeleton className="h-6 w-[300px]" />
+              <Skeleton className="h-4 w-[250px]" />
+            </div>
+            <div className="flex items-center justify-center gap-3">
+              <div className="space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden md:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden sm:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+              <div className="hidden lg:block space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-4 w-[70px]" />
+                <Skeleton className="h-4 w-[100px]" />
+                <Skeleton className="h-4 w-[150px]" />
+                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-4 w-[50px]" />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <section className="p-5 max-w-5xl mx-auto">
+          <BooksComponent
+            title="Current New York Times Bestsellers"
+            subTitle="
           The fiction and nonfiction that everyone’s talking about right now."
-          books={books as Book[]}
-        />
-      </section>
+            books={books as Book[]}
+          />
+        </section>
+      )}
 
       {/* Recommended Categories */}
-      <section className="p-5 max-w-5xl mx-auto">
-        <RecommendedCategories />
-      </section>
+      {loading ? (
+        <div className="block w-fit mx-auto">
+          <div className="flex flex-col gap-3 p-5">
+            <div className="mb-2 space-y-3">
+              <Skeleton className="h-6 w-[300px]" />
+              <Skeleton className="h-4 w-[250px]" />
+            </div>
+            <div className="flex items-center justify-center gap-3">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <Skeleton className="hidden md:block h-[125px] w-[250px] rounded-xl" />
+              <Skeleton className="hidden sm:block h-[125px] w-[250px] rounded-xl" />
+              <Skeleton className="hidden lg:block h-[125px] w-[250px] rounded-xl" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <section className="p-5 max-w-5xl mx-auto">
+          <RecommendedCategories />
+        </section>
+      )}
     </main>
   )
 }
