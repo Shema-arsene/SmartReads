0b6bf0d57a7748e1708d4c93bf308562534c32a5
Shema Arsene <aniyonkurushema@gmail.com>
2025-09-27 19:05:15 +0200

    Change e-books page layout

--- app/e-books/page.tsx
+++ app/e-books/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import React, { useEffect, useState } from "react"
 import { bookCategories } from "@/components/PublishBookForm"
+import BooksSkeleton from "@/components/BooksSkeleton"
 
 type Book = {
   _id: string
@@ -37,8 +38,7 @@ export default function AllBooksPage() {
       setBooks(data.books)
       setFilteredBooks(data.books)
     } catch (err: any) {
-      // setError(`Something went wrong: ${err.message}`)
-      setError("No Books Found")
+      setError(`Something went wrong: ${err.message}`)
     } finally {
       setLoading(false)
     }
@@ -57,21 +57,13 @@ export default function AllBooksPage() {
     }
   }, [activeCategory, books])
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center mt-20 h-screen">
-        <div className="w-16 h-16 border-4 border-[#c8553d] border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    )
-  }
-
-  if (error) {
-    return (
-      <div className="flex justify-center items-center mt-20 h-60">
-        <p className="text-red-600 text-xl">{error}</p>
-      </div>
-    )
-  }
+  // if (error) {
+  //   return (
+  //     <div className="flex justify-center items-center mt-20 h-60">
+  //       <p className="text-red-600 text-xl">{error}</p>
+  //     </div>
+  //   )
+  // }
 
   return (
     <section className="max-w-6xl mx-auto px-4 py-12 bg-[#fdfdfd]">
@@ -79,12 +71,12 @@ export default function AllBooksPage() {
         <h1 className="text-3xl font-bold text-[#c8553d]">E-Books Library</h1>
         <p className="text-lg font-medium">What interests you</p>
       </div>
-      <div className="flex items-center justify-between mb-8 py-10 px-6">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-3 mb-8 py-10 px-6">
         <form action="">
           <input
             type="text"
             placeholder="Search books, authors, genres..."
-            className="max-md:hidden md:w-xs lg:w-lg rounded-full border border-black px-5 py-2 text-gray-600"
+            className="md:w-xs lg:w-lg rounded-full border border-black px-5 py-2 text-gray-600"
           />
         </form>
 
@@ -108,40 +100,48 @@ export default function AllBooksPage() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {filteredBooks.map((book) => (
-          <div
-            key={book._id}
-            className="bg-white rounded-xl border border-[#f3f3f3] shadow-sm hover:shadow-md transition duration-300 overflow-hidden"
-          >
-            <Link href={`/e-books/${book._id}`}>
-              <img
-                src={book.imageUrl}
-                alt={book.title}
-                className="w-full h-56 object-cover transition duration-300 hover:opacity-90"
-              />
-            </Link>
+        {loading ? (
+          <BooksSkeleton />
+        ) : (
+          <>
+            {filteredBooks.map((book) => (
+              <div
+                key={book._id}
+                className="bg-white rounded-xl border border-[#f3f3f3] max-w-[350px] mx-auto shadow-sm hover:shadow-md transition duration-300 overflow-hidden"
+              >
+                <Link href={`/e-books/${book._id}`}>
+                  <img
+                    src={book.imageUrl}
+                    alt={book.title}
+                    className="w-full h-56 object-cover transition duration-300 hover:opacity-90"
+                  />
+                </Link>
 
-            <div className="p-4">
-              <Link href={`/e-books/${book._id}`}>
-                <h2 className="text-xl font-semibold text-[#c8553d] hover:underline mb-1">
-                  {book.title}
-                </h2>
-              </Link>
+                <div className="p-4">
+                  <Link href={`/e-books/${book._id}`}>
+                    <h2 className="text-xl font-semibold text-[#c8553d] hover:underline mb-1">
+                      {book.title}
+                    </h2>
+                  </Link>
 
-              <p className="text-[#588b8b] text-sm mb-1">By: {book.author}</p>
-              <p className="text-gray-500 text-xs uppercase tracking-wide mb-2">
-                {book.category}
-              </p>
-              <p className="text-sm text-gray-700 line-clamp-3 mb-3">
-                {book.description}
-              </p>
+                  <p className="text-[#588b8b] text-sm mb-1">
+                    By: {book.author}
+                  </p>
+                  <p className="text-gray-500 text-xs uppercase tracking-wide mb-2">
+                    {book.category}
+                  </p>
+                  <p className="text-sm text-gray-700 line-clamp-3 mb-3">
+                    {book.description}
+                  </p>
 
-              <p className="text-right text-[#f28f3b] font-semibold text-base">
-                ${book.price}
-              </p>
-            </div>
-          </div>
-        ))}
+                  <p className="text-right text-[#f28f3b] font-semibold text-base">
+                    ${book.price}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </section>
   )
